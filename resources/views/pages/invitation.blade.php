9d5268edbcb74ef46b96cb8560d9cfcd42997ee0
mambaur <alwiroziq1998@gmail.com>
2022-06-03 20:31:23 +0700

    update

--- resources/views/pages/invitation.blade.php
+++ resources/views/pages/invitation.blade.php
@@ -5,48 +5,106 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Undangan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
   </head>
   <body>
-    
-    <div class="row justify-content-center">
-      <div class="col-md-3 col-sm-12 bg-danger" style="overflow-y: scroll; height: 100vh; overflow-x: hidden;">
-        <div class="p-5 bg-danger">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
-        </div>
-        <div class="p-5 bg-warning">
-          Holla
+    <div class="container">
+
+      <div class="row justify-content-center">
+        <div class="col-md-6 col-sm-12 p-0 bg-danger position-relative">
+
+
+
+          <div class="position-fixed bg-success">
+            {{-- <div class="col-md-12 col-sm-12 p-0"> --}}
+              
+              <div class="row bg-light rounded-5 py-3 shadow">
+                <div class="col-md-3 col-sm-3 text-center">
+                  <i class="bi bi-house"></i>
+                  <div class="">
+                    Home
+                  </div>
+                </div>
+                <div class="col-md-3 col-sm-3 text-center">
+                  <i class="bi bi-house"></i>
+                  <div class="">
+                    Home
+                  </div>
+                </div>
+                <div class="col-md-3 col-sm-3 text-center">
+                  <i class="bi bi-house"></i>
+                  <div class="">
+                    Home
+                  </div>
+                </div>
+                <div class="col-md-3 col-sm-3 text-center">
+                  <i class="bi bi-house"></i>
+                  <div class="">
+                    Home
+                  </div>
+                </div>
+              </div>
+  
+            {{-- </div> --}}
+          </div>
+          
+          
+          <div class="row bg-danger">
+          <div class="row bg-danger">
+            <div class="col-md-12">
+              <div class="bg-warning"  style="height: 300px;">
+                {{-- Holla --}}
+              </div>
+              <div class="bg-danger" style="height: 300px;">
+                {{-- Holla --}}
+              </div>
+              <div class="bg-warning"  style="height: 300px;">
+                {{-- Holla --}}
+              </div>
+              <div class="bg-danger" style="height: 300px;">
+                {{-- Holla --}}
+              </div>
+
+            </div>
+
+          </div>
+          </div>
         </div>
       </div>
+
+      {{-- <div class="position-fixed"> --}}
+        {{-- <div class="row mb-3 justify-content-center">
+          <div class="col-md-6 col-sm-12 p-0">
+            <div class="row position-absolute  bg-light rounded-5 py-3 shadow">
+              <div class="col-md-3 col-sm-3 text-center">
+                <i class="bi bi-house"></i>
+                <div class="">
+                  Home
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-3 text-center">
+                <i class="bi bi-house"></i>
+                <div class="">
+                  Home
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-3 text-center">
+                <i class="bi bi-house"></i>
+                <div class="">
+                  Home
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-3 text-center">
+                <i class="bi bi-house"></i>
+                <div class="">
+                  Home
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div> --}}
+      {{-- </div> --}}
     </div>
 
 
